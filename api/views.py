5dec196f85c1570cf42c185faad0fd33a9bcfd84
Pavel Panchenko <mutedop@yandex.ru>
2021-06-03 07:10:15 +0800

    operating time by user

--- api/views.py
+++ api/views.py
@@ -25,25 +25,3 @@ class UserViewSet(viewsets.ModelViewSet):
         if request.method == 'GET':
             return self.retrieve(request)
         return self.update(request, partial=True)
-
-
-    # @action(detail=False, methods=['get', 'patch'],
-    #         permission_classes=(IsAuthenticated, ), url_path='me')
-    # def me(self, request, *args, **kwargs):
-    #     self.kwargs['username'] = self.request.user.username
-    #     if request.method == 'GET':
-    #         return self.retrieve(request)
-    #     if request.method == 'PATCH':
-    #         return self.update(request, partial=True)
-
-
-    # def me(self, request):
-    #     user = self.request.user
-    #     serializer = self.get_serializer(user)
-    #     if self.request.method == 'PATCH':
-    #         serializer = self.get_serializer(user,
-    #                                          data=request.data,
-    #                                          partial=True)
-    #         serializer.is_valid(raise_exception=True)
-    #         serializer.save()
-    #     return Response(serializer.data)
